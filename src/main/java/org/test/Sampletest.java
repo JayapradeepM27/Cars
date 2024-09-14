e7e5254e86c35b1353d765ce28a8ccd47c049262
JayapradeepM <jayapradeep00murugesan@gmail.com>
2024-09-14 19:30:36 +0530

    Modified

---
+++ src/main/java/org/test/Sampletest.java
@@ -0,0 +1,16 @@
+package org.test;
+class Sampletest {
+	private void method1() {
+		System.out.println("Method1");
+	}
+	private void method2() {
+	System.out.println("Method2");
+	}
+	public static void main(String[] args) {
+		Sampletest s = new Sampletest();
+		s.method1();
+		s.method2();
+		
+	}
+
+}
